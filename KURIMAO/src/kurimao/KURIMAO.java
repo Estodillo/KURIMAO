8037034167a921639f28335eef049f668d08cf23
adamd <adamd@192.168.0.127>
2024-09-19 16:15:23 +0800

    emma

--- KURIMAO/src/kurimao/KURIMAO.java
+++ KURIMAO/src/kurimao/KURIMAO.java
@@ -16,6 +16,7 @@ public class KURIMAO {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Kurimao");
+        System.out.println("Hello World");
     }
     
 }
